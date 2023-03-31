4eeaeaf0a3305d5845b37ca100506ed291c3b7a6
Belyse <belysenyampundu@gmail.com>
2023-03-31 15:30:10 +0300

    first commit

---
+++ math.py
@@ -0,0 +1,15 @@
+def add(a,b):
+    answer=a+b
+    return answer
+def substract(c,d):
+    answer1=c-d 
+    return answer1
+def divide(e,f):
+    answer2=e/f
+    return answer2
+def multiply(g,h):
+    answer3=g*h  
+    return answer3 
+def remainder(j,k):
+    answer4=j%k
+    return answer4
